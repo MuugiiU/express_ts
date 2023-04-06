7915952a07103bed08b983d55fbe54631bac849d
MuugiiU <classroom2leap@gmail.com>
2023-04-06 18:30:10 +0800

    pagination

--- controller/movies.controller.ts
+++ controller/movies.controller.ts
@@ -2,8 +2,17 @@ import { Request, Response } from "express";
 import Movie from "../model/Movie";
 
 const getMovies = async (req: Request, res: Response) => {
-  const movies = await Movie.find().limit(10);
-  res.status(200).json({ success: true, movies });
+  const limit = Number(req.query.limit) || 10;
+  const page = Number(req.query.page) || 0;
+
+  const total = await Movie.countDocuments();
+  const pageCount = Math.ceil(total / limit);
+  const skip = (page - 1) * limit;
+
+  const movies = await Movie.find().skip(skip).limit(limit);
+  res
+    .status(200)
+    .json({ success: true, movies, pagination: { total, pageCount, page } });
 };
 
 export { getMovies };
